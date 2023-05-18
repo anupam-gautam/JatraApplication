0ef690321f8a2ed7a58d9c8cc307f061909bc2ac
anupam-gautam <anupam.gautam46@gmail.com>
2023-05-18 22:03:35 +0545

    Calendar Partially Implemented

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -1,47 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using JatraApplication.Models.Calendar;
 
 namespace JatraApplication.Controllers
 {
     public class CalendarController : Controller
     {
+        //private string calendarData;
+
         [HttpGet]
         public IActionResult Index(int month = 1)
         {
+            string json;
+            CalendarData calendarInfo;
             using (StreamReader r = new StreamReader("../JatraApplication/CalendarJson/1.json"))
             {
-                string json = r.ReadToEnd();
-                List<CalendarData> calendarInfo = JsonConvert.DeserializeObject<List<CalendarData>>(json);
+                json = r.ReadToEnd();
+                calendarInfo = JsonConvert.DeserializeObject<CalendarData>(json);
             }
-            return PartialView();
+            ViewBag.calendarInfo = calendarInfo; // Corrected syntax
+            return PartialView("_calendarPreview");
+            return View("_calendarPreview");
         }
     }
-
-    public class Metadata
-    {
-        public string en { get; set; }
-        public string np { get; set; }
-    }
-
-    public class Day
-    {
-        public string n { get; set; }
-        public string e { get; set; }
-        public string t { get; set; }
-        public string f { get; set; }
-        public bool h { get; set; }
-        public int d { get; set; }
-    }
-
-    public class CalendarData
-    {
-        public Metadata metadata { get; set; }
-        public Day[] days { get; set; }
-        public string[] holiFest { get; set; }
-        public string[] marriage { get; set; }
-        public string[] bratabandha { get; set; }
-    }
-
-
-
 }
---
+++ Models/Calendar/CalendarData.cs
@@ -0,0 +1,28 @@
+﻿namespace JatraApplication.Models.Calendar
+{
+    public class Metadata
+    {
+        public string en { get; set; }
+        public string np { get; set; }
+    }
+
+    public class Day
+    {
+        public string n { get; set; }
+        public string e { get; set; }
+        public string t { get; set; }
+        public string f { get; set; }
+        public bool h { get; set; }
+        public int d { get; set; }
+    }
+
+    public class CalendarData
+    {
+        public Metadata metadata { get; set; }
+        public Day[] days { get; set; }
+        public string[] holiFest { get; set; }
+        public string[] marriage { get; set; }
+        public string[] bratabandha { get; set; }
+    }
+
+}
