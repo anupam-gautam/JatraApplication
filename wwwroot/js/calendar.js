b6e05abde4cb1b6277e679b10a4f70a91923fabf
anupam-gautam <anupam.gautam46@gmail.com>
2023-05-21 16:25:37 +0545

    Calendar fully implemented

---
+++ wwwroot/js/calendar.js
@@ -0,0 +1,31 @@
+
+
+const componentA = document.querySelector('.component-a');
+const componentB = document.querySelector('.component-b');
+
+componentA.addEventListener('mouseover', () => {
+    componentB.classList.add('highlight');
+    componentA.classList.add('highlight');
+});
+
+componentB.addEventListener('mouseover', () => {
+    componentB.classList.add('highlight');
+    componentA.classList.add('highlight');
+});
+
+componentA.addEventListener('mouseout', () => {
+    componentB.classList.remove('highlight');
+    componentA.classList.remove('highlight');
+});
+
+componentB.addEventListener('mouseout', () => {
+    componentB.classList.remove('highlight');
+    componentA.classList.remove('highlight');
+});
+
+
+
+function changeMonth(month) {
+    window.location.href = "/Home/Index?month=" + month;
+}
+
